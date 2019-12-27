6c19588f40b6c0c71a940cd92c499792e71b6d6b
jhaco <50258964+jhaco@users.noreply.github.com>
2019-12-27 15:25:15 -0600

    initialized PlaygroundContainers class

--- src/main/java/net/playground/PlaygroundClient.java
+++ src/main/java/net/playground/PlaygroundClient.java
@@ -2,6 +2,7 @@ package net.playground;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.playground.client.PlaygroundColors;
+import net.playground.client.PlaygroundContainers;
 import net.playground.client.PlaygroundRenderLayer;
 
 public class PlaygroundClient implements ClientModInitializer {
@@ -9,5 +10,6 @@ public class PlaygroundClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		PlaygroundRenderLayer.init();
 		PlaygroundColors.init();
+		PlaygroundContainers.init();
 	}
 }
